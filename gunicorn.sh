d7c4fd98c12a4b0a8827188802f81fd48728ef41
Arnolds Bogdanovs <arnolds.bogdanovs@gmail.com>
2018-04-24 13:55:13 +0300

    Gunicorn config changes

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,6 +2,6 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-exec gunicorn helloworld.wsgi:application \
+exec gunicorn dvaREST.dvaREST.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
